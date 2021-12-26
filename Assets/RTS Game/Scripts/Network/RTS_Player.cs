85482850451a062df7c94543028ff92268a98252
Jerrin Thomas <jerrin.thomas@myyhashstash.com>
2021-12-26 23:27:37 +0530

    Update 1

--- Assets/RTS Game/Scripts/Network/RTS_Player.cs
+++ Assets/RTS Game/Scripts/Network/RTS_Player.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Mirror;
+using TMPro;
+using System;
 
 namespace RTS.Network
 {
     public class RTS_Player : NetworkBehaviour
     {
-        [SyncVar]
+        [SerializeField]
+        private Renderer meshRenderer;
+        [SerializeField]
+        private TextMeshPro nameText;
+
+
+        [SyncVar(hook = nameof(HandlePlayerName))]
         [SerializeField]
         private string displayName = "Name";
 
-        [SyncVar]
+        [SyncVar(hook = nameof(HandlePlayerColor))]
         [SerializeField]
         private Color color = Color.white;
+
+
+        private void HandlePlayerColor(Color old, Color newColor)
+        {
+            meshRenderer.material.SetColor("_BaseColor", newColor);
+        }
+        private void HandlePlayerName(string old, string newName)
+        {
+            nameText.text = newName;
+        }
+
 
 
         [Server]
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # RTS-Game
+
+Remote Action
+- [Command] CmdDoSomething() 
+
+    For clients calling a method on the server
+
+- [ClientRpc] RpcDoSomething()
+
+    For the server calling on all the clients
+
+- [TargetRpc] TargetDoSomething()
+
+    For the server calling a method on a specific client
